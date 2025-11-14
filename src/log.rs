b5eed428e3dd9be042a9f521af5986485f544f98
Rahul Kodali <rahulmkodali@gmail.com>
2025-11-14 00:19:54 -0800

    feat: added WAL load function

--- src/log.rs
+++ src/log.rs
@@ -22,6 +22,55 @@ pub struct Wal{
     pub entries: Vector<LogEntry>,
 }
 
+impl Wal {
+    pub fn open(path: &str) -> Result<Wal> {
+        let file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(path)?;
+
+        let mut wal = Wal {
+            file,
+            entries: Vec::new(),
+        };
+
+        wal.load()?;
+        Ok(wal)
+    }
+
+    fn load(&mut self) -> Result<()> {
+        self.file.seek(SeekFrom::Start(0))?;
+        let mut buf = Vec::new();
+        self.file.read_to_end(&mut buf)?;
+
+        let mut cursor = std::io::Cursor::new(buf);
+
+        loop{
+
+            let mut len_buf = [0u8; 4];
+            if cursor.read_exact(&mut len_buf).is_err(){
+                break;
+            }
+
+            let len = u32::from_le_bytes(len_buf) as usize;
+
+            let mut data = vec![0u8; len];
+            cursor.read_exact(&mut data)?;
+
+            let entry: LogEntry = bincode::deserialize(&data)?;
+            self.entries.push(entry);
+        }
+        Ok(())
+    }
+
+    fn append(&mut self, entry: &LogEntry) -> Result<()> {
+        //add u32 log entry length header into 4 byte header
+        //add log entry itself serialized next
+    }
+
+}
+
 
 
 
